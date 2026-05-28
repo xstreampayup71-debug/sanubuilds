5fd4a059ecdef951f44ca925452c2867baadf5bb
xstreampayup71-debug <xstreampayup71@gmail.com>
2026-05-28 20:30:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -239,4 +239,5 @@ def train():
 # =========================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
